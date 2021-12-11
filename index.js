2353544a76933efc81d52c4ea28997b384198a5f
dino <dinos.yaw@gmail.com>
2021-12-11 16:16:54 +0800

    style: restyle code

--- index.js
+++ index.js
@@ -13,20 +13,20 @@ try {
     const gradlePath = core.getInput('gradlePath');
     console.log(`Gradle Path : ${gradlePath}`);
 
-    fs.readFile( gradlePath, 'utf-8', function( err, data ) {
+    fs.readFile(gradlePath, 'utf-8', function (err, data) {
 
-        versionCode = data.match( versionCodeRegexPattern ).pop()
-        versionName = data.match( versionNameRegexPattern ).pop()
-        while ( versionName.includes("\"") ) {
+        versionCode = data.match(versionCodeRegexPattern).pop()
+        versionName = data.match(versionNameRegexPattern).pop()
+        while (versionName.includes("\"")) {
             console.log("startsWith the \", need to delete it ")
-            versionName = versionName.replace( "\"", "" )
+            versionName = versionName.replace("\"", "")
         }
-        core.setOutput( "versionCode", versionCode )
-        core.setOutput( "versionName", versionName )
+        core.setOutput("versionCode", versionCode)
+        core.setOutput("versionName", versionName)
         console.log(`Get versionCode : ${versionCode}`);
         console.log(`Get versionName : ${versionName}`);
 
     });
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
